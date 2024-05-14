7fc4e2f43c457a4142b8c3e1bece7429a67fad4d
xaifuddin <xaifuddin@gmail.com>
2024-05-15 06:12:45 +0700

    Add files via upload

---
+++ Add Online.bat
@@ -0,0 +1,8 @@
+
+winget install -e --id 7zip.7zip
+winget install -e --id VideoLAN.VLC
+winget install -e --id Adobe.Acrobat.Reader.64-bit
+winget install -e --id Google.Chrome
+winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
+winget install -e --id Tonec.InternetDownloadManager
+winget install -e --id PowerSoftware.PowerISO
