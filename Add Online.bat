202e81fc40d378f646cbbc6dc1a75766d1e443de
xaifuddin <xaifuddin@gmail.com>
2024-05-15 06:47:22 +0700

    Update Add Online.bat

--- Add Online.bat
+++ Add Online.bat
@@ -8,6 +8,7 @@ winget install -e --id Adobe.Acrobat.Reader.64-bit
 winget install -e --id Google.Chrome
 winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
 winget install -e --id Tonec.InternetDownloadManager
+winget install -e --id Microsoft.OneDrive
 winget install -e --id PowerSoftware.PowerISO
 winget install -e --id WhatsApp.WhatsApp
 winget install -e --id COWON.jetAudio
